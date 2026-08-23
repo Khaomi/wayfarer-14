ed086a9cb456758c3b586fa3e5cd6fdfd73f01d7
Khaomi <46065349+Khaomi@users.noreply.github.com>
2026-08-23 22:17:13 +0700

    chore(organization): maintain similiar import list to frontier

--- Content.Client/Ghost/GhostSystem.cs
+++ Content.Client/Ghost/GhostSystem.cs
@@ -1,4 +1,5 @@
 using Content.Client.Movement.Systems;
+using Content.Client.UserInterface.Systems.Ghost.Widgets;
 using Content.Shared.Actions;
 using Content.Shared.Ghost;
 using Content.Shared.Mind;
@@ -14,7 +15,6 @@ using Robust.Shared.GameStates;
 using Robust.Shared.Timing;
 using Robust.Shared.Player;
 using Content.Client._Corvax.Respawn; // Frontier
-using Content.Client.UserInterface.Systems.Ghost.Widgets; // Frontier
 
 namespace Content.Client.Ghost
 {
